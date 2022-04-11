c574d54b1071771b222f7040184027415828d667
christianbc2021 <95112484+christianbc2021@users.noreply.github.com>
2022-04-10 23:56:40 -0500

    Update forward.h

--- forward.h
+++ forward.h
@@ -17,57 +17,142 @@ class ForwardList : public List<T> {
          	// TODO            
         }
 
-        T front(){
-            throw ("sin definir");
+     T front(){
+        if(head==nullptr){
+            return 0;
         }
+        else{
+           return head->data;
+        }
+    }
 
-        T back(){
-            throw ("sin definir");
+    T back(){
+        if(head==nullptr){
+            return 0;
         }
+        else{
+            Node<T>* temp= head;
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
 
-        void push_front(T data){
-            throw ("sin definir");
+            }
+            return temp->data;
         }
+    }
 
-        void push_back(T data){
-            throw ("sin definir");
-        }
+    void push_front(T data){
+        Node<T>* nuevo = new Node<T>;
+        nuevo->data = data;
+        nuevo->next = head;
+        head = nuevo;
+    }
 
-        T pop_front(){
-            throw ("sin definir");
-        }
+    void push_back(T data){
+        Node<T>* nuevo = new Node<T>;
+        nuevo->data = data;
+        Node<T>* temp = head;
+        while (temp->next != nullptr)
+        {temp = temp->next;}
+        temp->next = nuevo;
+        nuevo->next = nullptr;
+    }
 
-        T pop_back(){
-            throw ("sin definir");
-        }
+    T pop_front(){
+        Node<T> *temp = head;
+        head = head->next;
+        delete temp;
+    }
 
-        T insert(T data, int pos){
-            throw ("sin definir");
+    T pop_back(){
+        if(head->next == nullptr)
+        {
+            delete head;
+            head = nullptr;
         }
+        else {
 
-        bool remove(int pos){
-            throw ("sin definir");
+            Node<T>* temp = head;
+            while (temp->next->next != nullptr)
+            {  temp = temp->next;}
+            delete temp->next;
+            temp->next = nullptr;
         }
+    }
 
-        T& operator[](int pos){
-            throw ("sin definir");
-        }
+    T insert(T data, int pos){
+        Node<T>* nuevo = new Node<T>(data);
+        Node<T>* temp = head;
+        int i = 0;
+        while(i++ < pos - 1) {temp = temp->next;}
+        nuevo->next = temp->next;
+        temp->next = nuevo;
+    }
 
-        bool is_empty(){
-            throw ("sin definir");
+    bool remove(int pos){
+        if (head== nullptr){ return 0;}
+        else {
+            Node<T> *temp = head;
+            int i = 0;
+            while (i++ < pos - 1) { temp = temp->next; }
+            delete temp;
         }
+    }
 
-        int size(){
-            throw ("sin definir");
-        }
+    T& operator[](int pos){
 
-        void clear(){
-            throw ("sin definir");
+        Node<T>* temp = head;
+        int i = 0;
+        while(i++ < pos - 1) {temp = temp->next;}
+        return temp->data;
+
+
+    }
+
+    bool is_empty(){
+        if (head== nullptr){ return true;}
+        else { return false;}
+    }
+
+    int size(){
+        Node<T>* temp = head;
+        int i = 1;
+        if (head == nullptr){return 0;}
+        else {
+            while (temp->next != nullptr) {
+                temp = temp->next;
+                i++;
+            }
         }
-        
-        void sort(){
-            throw ("sin definir");
+        return i;
+    }
+
+    void clear(){
+        while(head != nullptr)
+        {
+            Node<T>* temp = head;
+            head = head->next;
+            delete temp;
         }
+    }
+
+    void sort(){
+
+        Node<T>* temp1 = head;
+        Node<T>* temp2 = temp1->next;
+        Node<T>* temp3 = temp2;
+        while (temp1->next != nullptr)
+        {
+          if (temp1->data > temp2->data & temp2!= nullptr)
+          {
+
+              temp2->data= temp1->data;
+              temp1->data= temp3-> data;
+              temp1=head;
+          }
+            temp1 = temp1->next;
+        }
+    }
 
         bool is_sorted(){
             throw ("sin definir");
